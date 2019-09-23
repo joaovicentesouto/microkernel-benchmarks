3db068c1d5fcc9141a1cfc615285a7f85d96fdc3
João Vicente Souto <joaovicentesouto@gmail.com>
2019-09-22 22:29:54 -0300

    kbench.h fence and atoi convert

--- include/kbench.h
+++ include/kbench.h
@@ -53,6 +53,87 @@
 	 */
 	#define HLINE "--------------------------------------------------------------------------------\n"
 
+	/**
+	 * @brief Generic fence.
+	 */
+	int fence_setup(int nclusters);
+	int fence_cleanup(void);
+	int fence(void);
+
+/*============================================================================*
+ * Convert Functions                                                          *
+ *============================================================================*/
+
+	/**
+	 *  Convert a string to a int.
+	 */
+	int __atoi(const char * str)
+	{
+		int result;
+
+		result = 0;
+
+		for (int i = 0; str[i] != '\0'; ++i)
+			result = (result * 10) + str[i] - '0';
+
+		return result;
+	}
+
+	/**
+	 *  Reverte a string.
+	 */
+	void reverse(char str[], int length)
+	{
+		char swap;
+		int start = 0;
+		int end = length -1;
+		while (start < end)
+		{
+			swap = *(str+start);
+			*(str+start) = *(str+end);
+			*(str+end) = swap;
+			start++;
+			end--;
+		}
+	}
+
+	/**
+	 *  Convert a int to a string.
+	 */
+	char* __itoa(int num, char* str, int base)
+	{
+		int i = 0;
+		bool isNegative = false;
+
+		if (num == 0)
+		{
+			str[i++] = '0';
+			str[i] = '\0';
+			return str;
+		}
+
+		if (num < 0 && base == 10)
+		{
+			isNegative = true;
+			num = -num;
+		}
+
+		while (num != 0)
+		{
+			int rem = num % base;
+			str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+			num = num/base;
+		}
+
+		if (isNegative)
+			str[i++] = '-';
+
+		str[i] = '\0';
+
+		reverse(str, i);
+
+		return str;
+	}
 
 /*============================================================================*
  * Memory Functions                                                           *
