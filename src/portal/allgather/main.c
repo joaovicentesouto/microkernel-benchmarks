ff9384ed3efc3bf00eeaf4dba66701dd361b4261
João Vicente Souto <joaovicentesouto@gmail.com>
2019-09-30 16:46:26 -0300

    portal allgather updates

--- src/portal/allgather/main.c
+++ src/portal/allgather/main.c
@@ -30,6 +30,9 @@
 
 #define MESSAGE_MSG_MAX (1024)
 
+static struct final_results results;
+static char message[PROCESSOR_CLUSTERS_NUM * MESSAGE_MSG_MAX];
+
 void do_work(int nodes[], int nnodes, int index, int message_size)
 {
 	int local;
@@ -37,60 +40,62 @@ void do_work(int nodes[], int nnodes, int index, int message_size)
 	int write_to;
 	int portal_in;
 	int portal_out;
-	char message[PROCESSOR_CLUSTERS_NUM * MESSAGE_MSG_MAX];
 
 	local = nodes[index];
 
 	KASSERT((portal_in = kportal_create(local)) >= 0);
 
-	for (unsigned i = 0; i < NITERATIONS; ++i)
-	{
-		/* Cleans the buffer. */
-		kmemset(message, (-1), nnodes * message_size);
-
-		/* Prepares the transfer data. */
-		kmemset(&message[index * message_size], (char) local, message_size);
-
-		for (int j = 1; j < nnodes; ++j)
+		for (unsigned i = 0; i < NITERATIONS; ++i)
 		{
-			read_from = (index - j) < 0 ? (nnodes + (index - j)) : (index - j);
-			write_to  = (index + j) % nnodes;
+			/* Cleans the buffer. */
+			kmemset(message, (-1), nnodes * message_size);
 
-			KASSERT((portal_out = kportal_open(local, nodes[write_to])) >= 0);
+			/* Prepares the transfer data. */
+			kmemset(&message[index * message_size], (char) local, message_size);
 
-				barrier();
+			for (int j = 1; j < nnodes; ++j)
+			{
+				read_from = (index - j) < 0 ? (nnodes + (index - j)) : (index - j);
+				write_to  = (index + j) % nnodes;
 
-				KASSERT(kportal_allow(portal_in, nodes[read_from]) == 0);
-				KASSERT(
-					kportal_aread(
-						portal_in,
-						&message[read_from * message_size],
-						message_size
-					) == message_size
-				);
+				KASSERT((portal_out = kportal_open(local, nodes[write_to])) >= 0);
 
+					barrier();
+
+					KASSERT(kportal_allow(portal_in, nodes[read_from]) == 0);
 					KASSERT(
-						kportal_write(
-							portal_out,
-							&message[index * message_size],
+						kportal_aread(
+							portal_in,
+							&message[read_from * message_size],
 							message_size
 						) == message_size
 					);
 
-				KASSERT(kportal_wait(portal_in) == 0);
+						KASSERT(
+							kportal_write(
+								portal_out,
+								&message[index * message_size],
+								message_size
+							) == message_size
+						);
 
-			KASSERT(kportal_close(portal_out) == 0);
+					KASSERT(kportal_wait(portal_in) == 0);
+
+				KASSERT(kportal_close(portal_out) == 0);
+			}
+
+			for (int j = 0; j < nnodes; ++j)
+			{
+				char value = (char) nodes[j];
+				char * curr_message = &message[(j * message_size)];
+
+				for (int k = 0; k < message_size; ++k)
+					KASSERT(curr_message[k] == value);
+			}
 		}
 
-		for (int j = 0; j < nnodes; ++j)
-		{
-			char value = (char) nodes[j];
-			char * curr_message = &message[(j * message_size)];
-
-			for (int k = 0; k < message_size; ++k)
-				KASSERT(curr_message[k] == value);
-		}
-	}
+		KASSERT(kportal_ioctl(portal_in, PORTAL_IOCTL_GET_LATENCY, &results.latency) == 0);
+		KASSERT(kportal_ioctl(portal_in, PORTAL_IOCTL_GET_VOLUME, &results.volume) == 0);
 
 	KASSERT(kportal_unlink(portal_in) == 0);
 }
@@ -108,44 +113,28 @@ void do_work(int nodes[], int nnodes, int index, int message_size)
 int main(int argc, const char *argv[])
 {
 	int index;
-	int ncclusters;
-	int nioclusters;
-	int message_size;
+	struct initial_arguments _args;
 	int nodes[PROCESSOR_CLUSTERS_NUM];
 
-	if (argc != 3)
-	{
-		kprintf("[portal][allgather] Invalid arguments! (%d)", argc);
-		return (-EINVAL);
-	}
+	exchange_arguments(argc, argv, &_args);
 
-	nioclusters  = atoi(argv[0]);
-	ncclusters   = atoi(argv[1]);
-	message_size = atoi(argv[2]);
-
-	if (ncclusters > 16 || message_size == 0)
-	{
-		kprintf("[portal][allgather] Bad arguments! (%d, %d)", ncclusters, message_size);
-		return (-EINVAL);
-	}
-
-	build_node_list(nioclusters, ncclusters, nodes);
+	build_node_list(_args.nioclusters, _args.ncclusters, nodes);
 
 	/* Filters the clusters involved. */
 	if (cluster_is_io())
 	{
-		if (cluster_get_num() >= nioclusters)
+		if (cluster_get_num() >= _args.nioclusters)
 			return (0);
 	}
 	else
 	{
-		if (cluster_get_num() >= (PROCESSOR_IOCLUSTERS_NUM + ncclusters))
+		if (cluster_get_num() >= (PROCESSOR_IOCLUSTERS_NUM + _args.ncclusters))
 			return (0);
 	}
 
 	/* Finds the node list index of the current cluster. */
 	index = -1;
-	for (int i = 0; i < (nioclusters + ncclusters); ++i)
+	for (int i = 0; i < (_args.nioclusters + _args.ncclusters); ++i)
 	{
 		if (nodes[i] == knode_get_num())
 		{
@@ -155,22 +144,43 @@ int main(int argc, const char *argv[])
 	}
 	KASSERT(index != -1);
 
-	kprintf(HLINE);
+	barrier_setup(_args.nioclusters, _args.ncclusters);
 
-	barrier_setup(nioclusters, ncclusters);
+	if (cluster_get_num() == PROCESSOR_CLUSTERNUM_MASTER)
+		kprintf(HLINE);
 
 		/* Runs the kernel. */
-		do_work(nodes, (nioclusters + ncclusters), index, message_size);
+		do_work(nodes, (_args.nioclusters + _args.ncclusters), index, _args.message_size);
 
+	if (cluster_get_num() == PROCESSOR_CLUSTERNUM_MASTER)
 		kprintf("[portal][allgather] Successfuly completed.");
 
 		barrier();
 
+		if (cluster_get_num() == PROCESSOR_CLUSTERNUM_MASTER)
+		{
+			uint64_t l0 = results.latency;
+			uint64_t v0 = results.volume;
+
+			receive_results(((_args.nioclusters - 1) + _args.ncclusters), &results);
+
+			results.latency = results.latency < l0 ? l0 : results.latency;
+			results.volume  = results.volume < v0  ? v0 : results.volume;
+
+			print_results((_args.nioclusters + _args.ncclusters), NITERATIONS, &results);
+		}
+		else
+			send_results(&results);
+
+		barrier();
+
+	if (cluster_get_num() == PROCESSOR_CLUSTERNUM_MASTER)
 		kprintf("[portal][allgather] Exit.");
 
 	barrier_cleanup();
 
-	kprintf(HLINE);
+	if (cluster_get_num() == PROCESSOR_CLUSTERNUM_MASTER)
+		kprintf(HLINE);
 
 	return (0);
 }
